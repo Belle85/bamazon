d5ac94d47f91bb73a1d1cdf1cce3a409d04a3355
Belle <bebold.bebelle@gmail.com>
2018-08-03 15:38:03 -0400

    Scrated what I had to only keep the start function.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,47 +46,20 @@ connection.query("SELECT * FROM products", (err, res) => {
 
 // ---------------------------------------------------------------
 
-// TODO: ask them the ID of the product they would like to buy.
+// Ask users the ID of the product they would like to buy.
 function start() {
-  connection.query("SELECT * FROM products", function (err, results) {
-    if (err) throw err;
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What product would you like to buy? Enter the product ID.",
-          name: "product_id",
-        },
-        // TODO: ask how many units of the product they would like to buy.
-        {
-          type: "input",
-          message: "How many units of this product would you like to buy?",
-          name: "units",
-          default: 1,
-          validate: function (value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "product_id",
+        message: "What product would you like to buy? Enter the product ID.",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
           }
+          return false;
         }
-      ])
-      .then(function (answer) {
-        var userProduct;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
-            chosenItem = results[i];
-          }
-        }
-        console.log("You would like to buy " + answer.units + " of " + userProduct);
-      });
-  })}
-
-// TODO: check if your store has enough of the product to meet the customer's request.
-
-
-
-// TODO: If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through
-
-
-
+      },
+    ])
+}

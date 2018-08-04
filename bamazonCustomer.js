ad274a74afefdac2f08ac92ba24930e9e50eba8a
Belle <bebold.bebelle@gmail.com>
2018-08-04 12:56:50 -0400

    Worked on my start function and wrote .then method to obtain and display data from my DB related to the item ID entered by user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,7 +39,7 @@ figlet("Belle's Store", function (err, data) {
 
 //USE OF TABLEFY TO DISPLAY DATABASE AS TABLE IN NODE.JS
 let table = new Tablefy()
-connection.query("SELECT * FROM products", (err, res) => {
+connection.query("SELECT * FROM products", function(err, res) {
   table.draw(res);
   start()
 });
@@ -50,9 +50,11 @@ connection.query("SELECT * FROM products", (err, res) => {
 function start() {
   inquirer
     .prompt([
+
+      // FIRST PROMPT QUESTION
       {
         type: "input",
-        name: "item_id",
+        name: "itemID",
         message: "What product would you like to buy? Enter the product ID.",
         validate: function (value) {
           if (isNaN(value) === false) {
@@ -61,5 +63,22 @@ function start() {
           return false;
         }
       },
+
+      // SECOND PROMPT QUESTION
+      {
+        type: "input",
+        name: "orderQTTY",
+        message: " How many units of this item would you like to purchase?",
+        default: 1
+      }
     ])
+    .then (function(answer){
+      connection.query("SELECT * FROM products WHERE item_id = " + answer.itemID, function(err, response){
+        if (err) throw err;
+       var productPurchase = response[0].product_name;
+       console.log("You wish to order "+ answer.orderQTTY + " unit(s) of " + productPurchase);
+
+      })
+    })
 }
+
